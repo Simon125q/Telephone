17b6ffd8fccf19c5493480c690c40782ca7bd0e0
Simon125q <106029626+Simon125q@users.noreply.github.com>
2023-11-12 22:19:16 +0100

    started implementation of telephon entry  and person classes

--- Person.java
+++ Person.java
@@ -1,4 +1,14 @@
 public class Person extends TelephoneEntry{
-    
-    Person(String firstName, String lastName, )
+
+    String lastName;
+
+    Person(String firstName, String lastName, String countryCode, String localNumber, String street, String city) {
+        super(firstName, street, city, countryCode, localNumber);
+        this.lastName = lastName; 
+    }
+
+    void description() {
+        System.out.println(name + " " + lastName);
+        System.out.println(address);
+    }
 }
--- TelephoneEntry.java
+++ TelephoneEntry.java
@@ -3,6 +3,10 @@ public abstract class TelephoneEntry
     String name;
     String address;
 
+    TelephoneEntry(String name, String street, String city, String countryCode, String localNumber) {
+        this.name = name;
+        this.address = city + ", " + street;
+    }
 
     abstract void description();
 }
